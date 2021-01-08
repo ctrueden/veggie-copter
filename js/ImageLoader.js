0f4d95bb497c4efe320132ea6a625f0669f681a6
Curtis Rueden <ctrueden@wisc.edu>
2021-01-08 11:03:46 -0600

    Begin converting ImageLoader logic

--- js/ImageLoader.js
+++ js/ImageLoader.js
@@ -1,41 +1,26 @@
-package net.restlesscoder.heli;
+const PRELOAD = [
+  "alex1.png", "alex2.png", "alex3.png",
+  "alex-boss1.png",
+  "paul1.png", "paul2.png", "paul3.png",
+  "paul-boss1.png"
+];
 
-import java.awt.image.BufferedImage;
-import java.io.IOException;
-import java.util.Hashtable;
-import javax.imageio.ImageIO;
-
-public class ImageLoader {
-
-  protected static final String[] PRELOAD = {
-    "alex1.png", "alex2.png", "alex3.png",
-    "alex-boss1.png",
-    "paul1.png", "paul2.png", "paul3.png",
-    "paul-boss1.png"
-  };
-
-  protected Hashtable imageHash;
-  protected int nextId;
-
-  public ImageLoader() {
-    imageHash = new Hashtable();
+class ImageLoader {
+  constructor() {
+    this.images = {};
     for (int i=0; i<PRELOAD.length; i++) loadImage(PRELOAD[i]);
   }
 
-  public BufferedImage getImage(String filename) {
-    BufferedImage image = (BufferedImage) imageHash.get(filename);
-    if (image == null) {
-      image = loadImage(filename);
-      imageHash.put(filename, image);
-    }
+  getImage(path) {
+    if (path in this.images) return this.images[path];
+    var image = this.loadImage(path);
+    this.images[path] = image;
     return image;
   }
 
-  protected BufferedImage loadImage(String filename) {
-    BufferedImage image = null;
-    try { image = ImageIO.read(getClass().getResource(filename)); }
-    catch (IOException exc) { exc.printStackTrace(); }
-    return ImageTools.makeCompatible(image, null);
+  loadImage(path) {
+    var image = new Image();
+    image.src = path;
+    return image;
   }
-
 }
