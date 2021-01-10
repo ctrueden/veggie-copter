82f19dec2e640a01fb7ad26cfba6ad1e2a1140da
Curtis Rueden <ctrueden@wisc.edu>
2021-01-09 18:55:30 -0600

    Migrate ImageLoader into util

--- js/util.js
+++ js/util.js
@@ -1,5 +1,35 @@
-// Utility functions.
+// Utility constants, functions and classes.
+
+DARK_RED = "rgb(48, 0, 0)";
 
 function equalsIgnoreCase(s1, s2) {
     return s1.localeCompare(s2, undefined, { sensitivity: 'base' }) === 0;
+}
+
+function drawOutlinedRect(ctx, color, x, y, width, height) {
+  ctx.fillColor = color;
+  ctx.fillRect(x, y, width - 1, height - 1);
+  ctx.beginPath();
+  ctx.strokeColor = "white";
+  ctx.rect(x, y, width - 1, height - 1);
+  ctx.stroke();
+}
+
+class ImageLoader {
+  constructor() {
+    this.images = {};
+  }
+
+  getImage(path) {
+    if (path in this.images) return this.images[path];
+    var image = this.loadImage(path);
+    this.images[path] = image;
+    return image;
+  }
+
+  loadImage(path) {
+    var image = new Image();
+    image.src = path;
+    return image;
+  }
 }
