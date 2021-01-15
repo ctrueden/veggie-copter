3a0080666ee3e2d314b5442a0502d266638bcb4e
Curtis Rueden <ctrueden@wisc.edu>
2021-01-14 19:53:11 -0600

    Cache generated powerup sprites

--- js/powerup.js
+++ js/powerup.js
@@ -20,7 +20,7 @@ class PowerUpMovement extends MovementStyle {
       }
       else {
         index++;
-        if (index == this.thing.sprites.length - 1) this.dir = !this.dir;
+        if (index == this.thing.pulse - 1) this.dir = !this.dir;
       }
       this.thing.activateSprite(index);
     }
@@ -59,30 +59,32 @@ class PowerUp extends Thing {
     super(game);
     this.type = ThingTypes.POWER_UP;
     this.weapon = weapon;
+    this.pulse = 10; // Number of pulsation sprites.
 
     // create power-up images
     var sprites = {};
-    var pulse = 10; // Number of ticks of pulsation.
     var col = this.weapon == null ? Colors.White : this.weapon.color;
-    var rHalf = red(col) / 2;
-    var gHalf = green(col) / 2;
-    var bHalf = blue(col) / 2;
-    for (var i=0; i<pulse; i++) {
-      var r = rHalf + rHalf * (i + 1) / pulse;
-      var g = gHalf + gHalf * (i + 1) / pulse;
-      var b = bHalf + bHalf * (i + 1) / pulse;
-      var image = makeImage(size, size);
-      var ctx = context2d(image);
-      var median = size / 2;
-      for (var rad=median; rad>=1; rad--) {
-        var a = (median - rad) / median;
-        ctx.fillStyle = color(r, g, b, a);
-        ctx.ellipse(median, median, rad, rad, 0, 0, 2 * Math.PI);
-        ctx.fill();
-      }
-      var sprite = new Sprite(image);
-      sprite.addBox(new BoxInsets(1, 1, 1, 1));
-      sprites[i] = sprite;
+    for (var i=0; i<this.pulse; i++) {
+      sprites[i] = this.game.retrieve(`powerup-${size}-${col}`, this, obj => {
+        var rHalf = red(col) / 2;
+        var gHalf = green(col) / 2;
+        var bHalf = blue(col) / 2;
+        var r = rHalf + rHalf * (i + 1) / this.pulse;
+        var g = gHalf + gHalf * (i + 1) / this.pulse;
+        var b = bHalf + bHalf * (i + 1) / this.pulse;
+        var image = makeImage(size, size);
+        var ctx = context2d(image);
+        var median = size / 2;
+        for (var rad=median; rad>=1; rad--) {
+          var a = (median - rad) / median;
+          ctx.fillStyle = color(r, g, b, a);
+          ctx.ellipse(median, median, rad, rad, 0, 0, 2 * Math.PI);
+          ctx.fill();
+        }
+        var sprite = new Sprite(image);
+        sprite.addBox(new BoxInsets(1, 1, 1, 1));
+        return sprite;
+      });
     }
     this.setSprites(sprites, 0);
 
